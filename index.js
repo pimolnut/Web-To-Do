eedb68f69c15caec885e242a20d4a52c6196b56c
pimolnut <64010605@kmitl.ac.th>
2023-12-11 14:58:23 +0700

    text box

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // Select the heading element
-var heading = document.querySelector('h1');
+let discountButton = document.getElementById('discount-button');
+let message = document.getElementById('message');
 
+
+function showMessage() {
+    message.innerHTML = 'time out'
+}
+
+discountButton.addEventListener('click', showMessage);
 // Change the text content of the heading
 heading.textContent = 'Hello, JavaScript!';
